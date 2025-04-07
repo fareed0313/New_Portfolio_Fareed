4ce4aaad4f9a8c3a834b5c8d9c33ac129adbd96c
Chaitanya Sai Meka <marvelchaitanya@gmail.com>
2025-04-07 13:48:40 +0530

    ninth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
@@ -6,24 +6,41 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Footer from './components/Footer';
+import Loader from './components/Loader';
+import EnterScreen from './components/EnterScreen';
+import Footer from './components/Footer'; 
 
 function App() {
+  const [started, setStarted] = useState(false);
+  const [showLoader, setShowLoader] = useState(false);
+
+  const handleStart = () => {
+    setStarted(true);
+    setShowLoader(true);
+  };
+
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <>
+      {!started ? (
+        <EnterScreen onEnter={handleStart} />
+      ) : showLoader ? (
+        <Loader onComplete={() => setShowLoader(false)} />
+      ) : (
+        <Router>
+          <div className="bg-white dark:bg-black">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Hero />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+            <Footer />
+          </div>
+        </Router>
+      )}
+    </>
   );
 }
 
 export default App;
-
