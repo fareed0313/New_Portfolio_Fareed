9ea0309e6ebd4f22f0e5b75684d653562221ca6e
Chaitanya Sai Meka <marvelchaitanya@gmail.com>
2025-04-08 19:44:10 +0530

    15 commit

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
-
+import { cn } from "../lib/utils";
 
 const Skills = () => {
   return (
+    <div className="relative h-screen w-full overflow-hidden">
+      {/* Background Grid */}
+      <div
+        className={cn(
+          "absolute inset-0 z-0 pointer-events-none",
+          "[background-size:20px_20px]",
+          "[background-image:radial-gradient(#404040_1px,transparent_1px)]"
+        )}
+      />
 
-    <div className="coming-soon-text h-[100vh] flex justify-center items-center">
-    Under Construction 🏗️
-  </div>
-  
+      {/* Radial Mask Overlay */}
+      <div className="absolute inset-0 z-10 pointer-events-none bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
 
-
+      {/* Under Construction Text */}
+      <div className=" coming-soon-text relative z-20 h-full flex justify-center items-center text-white text-2xl">
+        Under Construction 🏗️
+      </div>
+    </div>
   );
 };
 
