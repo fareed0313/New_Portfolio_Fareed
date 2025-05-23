9908e77b9ad01615c29a4a8be9c65321f3aeea04
Chaitanya Sai Meka <marvelchaitanya@gmail.com>
2025-05-23 17:24:31 +0530

    23-05-2025-03

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
 import { cn } from "../lib/utils"; 
+import { FaGithub } from 'react-icons/fa';
 
 const projects = [
   {
@@ -128,6 +129,25 @@ const Projects = () => {
           </CardContainer>
         ))}
       </div>
+      <div>
+      <a 
+      href="https://github.com/ChaitanyaSai-Meka"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-base font-semibold leading-6  text-white inline-block">
+        <span className="absolute inset-0 overflow-hidden rounded-full">
+          <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+        </span>
+        <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-2 px-7 ring-1 ring-white/10 ">
+          <span>
+            For More
+          </span>
+          <FaGithub className="h-6 w-6 text-white" />
+        </div>
+        <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+      </a>
+      </div>
+
     </div>
   );
 };
